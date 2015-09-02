5bbef5f828a084ccb8a0b5e9fb077215862264f9
Skylor R Schermer <skyschermer@gmail.com>
2015-09-02 10:32:02 -0500

    WIP: Removed dead code.

--- src/lib.rs
+++ src/lib.rs
@@ -6,34 +6,6 @@ pub mod metric;
 pub mod common;
 pub mod simple;
 
-// type TreeItem = f64;
-
-// impl common::Metric<f64> for TreeItem {
-//     fn distance(self, rhs: f64) -> f64 {
-//         (rhs - self).abs() as f64
-//     }
-// }
-
-// impl<'a> common::Metric<f64> for &'a TreeItem {
-//     fn distance(self, rhs: f64) -> f64 {
-//         (rhs - self).abs() as f64
-//     }
-// }
-
-// impl<'b> common::Metric<&'b f64> for TreeItem {
-//     fn distance(self, rhs: &'b f64) -> f64 {
-//         (rhs - self).abs() as f64
-//     }
-// }
-
-// impl<'a, 'b> common::Metric<&'b f64> for &'a TreeItem {
-
-//     fn distance(self, rhs:&'b f64) -> f64 {
-//         (rhs - self).abs() as f64
-//     }
-// }
-
-
 
 #[test]
 fn test_simple_cover_tree() {
--- src/main.rs
+++ src/main.rs
@@ -5,37 +5,37 @@ pub mod metric;
 pub mod simple;
 pub mod common;
 
-// use simple::CoverTree;
-// use common::NearestNeighbor;
-// use treedisplay::TreeDisplay;
+use simple::CoverTree;
+use common::NearestNeighbor;
+use treedisplay::TreeDisplay;
 
 
 #[allow(dead_code)]
 fn main() {
 
-    // let mut ct: CoverTree<TreeItem> = CoverTree::new();
+    let mut ct: CoverTree<metric::MetricF64> = CoverTree::new();
 
-    // let data: &[f64; 12] = &[
-    //     1.0, 
-    //     10.0, 
-    //     100.0, 
-    //     122.0, 
-    //     123.0, 
-    //     144.0, 
-    //     20.0, 
-    //     25.0, 
-    //     35.0, 
-    //     78.0, 
-    //     89.0,
-    //     90.0 
-    // ];
-    // for point in data.iter() {
-    //     ct.insert(*point);
-    // }
-    // println!("{}\n", ct.tree_string());
+    let data: &[f64; 12] = &[
+        1.0, 
+        10.0, 
+        100.0, 
+        122.0, 
+        123.0, 
+        144.0, 
+        20.0, 
+        25.0, 
+        35.0, 
+        78.0, 
+        89.0,
+        90.0 
+    ];
+    for point in data.iter() {
+        ct.insert(*point);
+    }
+    println!("{}\n", ct.tree_string());
 
 
-    // println!("Nearest to 66: {}", ct.find_nearest(66.0).unwrap());
-    // println!("Nearest to 93: {}", ct.find_nearest(93.0).unwrap());
-    // println!("Nearest to 94: {}", ct.find_nearest(94.0).unwrap());
+    println!("Nearest to 66: {}", ct.find_nearest(66.0).unwrap());
+    println!("Nearest to 93: {}", ct.find_nearest(93.0).unwrap());
+    println!("Nearest to 94: {}", ct.find_nearest(94.0).unwrap());
 }
