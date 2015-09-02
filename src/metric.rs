a13d4ba93204db78ff938e70c412954617ecc298
Skylor R Schermer <skyschermer@gmail.com>
2015-09-02 10:30:41 -0500

    WIP: Fixed output type on distance functions. Added other primitive metric types.

--- src/metric.rs
+++ src/metric.rs
@@ -6,35 +6,38 @@ pub trait Metric<RHS=Self> {
 
 
 macro_rules! make_metric {
-	($t:ty, $name:ident) => (
+	($name:ident, $t:ty) => (
 		pub type $name = $t;
 
 		impl Metric<$t> for $name {
-		    fn distance(self, rhs: $t) -> $t {
-		        (rhs - self).abs() as $t
+		    fn distance(self, rhs: $t) -> f64 {
+		        (rhs - self).abs() as f64
 		    }
 		}
 
 		impl<'a> Metric<$t> for &'a $name {
-		    fn distance(self, rhs: $t) -> $t {
-		        (rhs - self).abs() as $t
+		    fn distance(self, rhs: $t) -> f64 {
+		        (rhs - self).abs() as f64
 		    }
 		}
 
 		impl<'b> Metric<&'b $t> for $name {
-		    fn distance(self, rhs: &'b $t) -> $t {
-		        (rhs - self).abs() as $t
+		    fn distance(self, rhs: &'b $t) -> f64 {
+		        (rhs - self).abs() as f64
 		    }
 		}
 
 		impl<'a, 'b> Metric<&'b $t> for &'a $name {
 
-		    fn distance(self, rhs:&'b $t) -> $t {
-		        (rhs - self).abs() as $t
+		    fn distance(self, rhs:&'b $t) -> f64 {
+		        (rhs - self).abs() as f64
 		    }
 		}
 	)
 }
 
 
-make_metric!(f64, MetricF64);
+make_metric!(MetricI32, i32);
+make_metric!(MetricI64, i64);
+make_metric!(MetricF32, f32);
+make_metric!(MetricF64, f64);
