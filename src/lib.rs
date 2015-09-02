b8c62979de9ac0938cd761a060c07656b3f4b839
Skylor R Schermer <skyschermer@gmail.com>
2015-09-02 13:00:06 -0500

    WIP: Started implementing tests.

--- src/lib.rs
+++ src/lib.rs
@@ -8,36 +8,47 @@ pub mod simple;
 
 
 
-
-#[test]
-fn test_simple_cover_tree() {
+#[cfg(test)]
+mod tests {
     use metric::MetricF64;
     use simple::CoverTree;
     use common::NearestNeighbor;
-    let mut ct: CoverTree<MetricF64> = CoverTree::new();
 
-    let data: &[f64; 12] = &[
-        1.0, 
-        10.0, 
-        100.0, 
-        122.0, 
-        123.0, 
-        144.0, 
-        20.0, 
-        25.0, 
-        35.0, 
-        78.0, 
-        89.0,
-        90.0 
-    ];
-    for point in data.iter() {
-        ct.insert(*point);
+    fn test_f64_data() -> Vec<f64> {
+        vec![
+            1.0, 
+            10.0, 
+            100.0, 
+            122.0, 
+            123.0, 
+            144.0, 
+            20.0, 
+            25.0, 
+            35.0, 
+            78.0, 
+            89.0,
+            90.0
+        ]
     }
 
-    assert_eq!(ct.find_nearest(10.0), Some(&10.0));
-    assert_eq!(ct.find_nearest(94.0), Some(&90.0));
-    assert_eq!(ct.find_nearest(80.0), Some(&78.0));
-    assert_eq!(ct.find_nearest(3.0), Some(&1.0));
-    assert_eq!(ct.find_nearest(121.0), Some(&122.0));
-}
+    #[test]
+    fn test_empty_tree() {
+        let ct: CoverTree<MetricF64> = CoverTree::new();
+    }
 
+    #[test]
+    fn test_from_items() {
+        let ct: CoverTree<MetricF64> = CoverTree::from_items(test_f64_data().into_iter());
+    }
+
+    #[test]
+    fn test_manual_insert() {
+        let mut ct: CoverTree<MetricF64> = CoverTree::new();
+
+        let data = &test_f64_data();
+
+        for point in data.iter() {
+            ct.insert(*point);
+        }
+    }
+}
